492330d6db115f0c6920ebaf561dc1b44b957f5f
yxzuo <yxzuo@ctrip.com>
2017-08-11 16:20:44 +0800

    add reconstruct

--- src/main/java/BinaryTree.java
+++ src/main/java/BinaryTree.java
@@ -1,8 +1,5 @@
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Created by yxzuo on 2017/8/3.
@@ -130,4 +127,93 @@ public class BinaryTree{
         root.right = tmp;
         return root;
     }
+
+    /**
+     * 根据先序遍历和中序遍历数组重构二叉树
+     * @param pre 先序遍历数组
+     * @param in 中序遍历数组
+     * @return 二叉树根节点
+     */
+    public static TreeNode reconstructByPreIn(int [] pre, int [] in) {
+        if(pre == null || in == null || 0 == pre.length || 0 == in.length || pre.length != in.length) return null;
+        TreeNode root = new TreeNode(pre[0]);
+
+        int index = -1;
+        for(int i = 0; i < in.length; ++i){
+            if(root.val == in[i]){
+                index = i;
+                break;
+            }
+        }
+
+        int[] leftPre = Arrays.copyOfRange(pre, 1, index + 1);
+        int[] leftIn = Arrays.copyOfRange(in, 0, index);
+
+        int[] rightPre = Arrays.copyOfRange(pre, index + 1, pre.length);
+        int[] rightIn = Arrays.copyOfRange(in, index + 1, in.length);
+
+        root.left = reconstructByPreIn(leftPre, leftIn);
+        root.right = reconstructByPreIn(rightPre, rightIn);
+        return root;
+    }
+
+    public static TreeNode reconstructByPostIn(int [] post, int [] in) {
+        if(post == null || in == null || post.length == 0 || in.length == 0 || post.length != in.length) return null;
+        int len = post.length;
+        TreeNode root = new TreeNode(post[len - 1]);
+        int index = -1;
+        for (int i = 0; i < len; i++) {
+            if(root.val == in[i]){
+                index = i;
+                break;
+            }
+        }
+        int[] leftIn = Arrays.copyOfRange(in, 0 , index);
+        int[] rightIn = Arrays.copyOfRange(in, index + 1, len);
+        int[] leftPost = Arrays.copyOfRange(post, 0, index);
+        int[] rightPost = Arrays.copyOfRange(post, index, len - 1);
+
+        root.left = reconstructByPostIn(leftPost, leftIn);
+        root.right = reconstructByPostIn(rightPost, rightIn);
+        return root;
+    }
+
+    public static TreeNode reconstructByPostIn_WithoutExtraSpace(int [] post, int [] in) {
+        if(post == null || in == null || post.length == 0 || in.length == 0 || post.length != in.length) return null;
+        TreeNode root = reconstructByPostInHelper(post, 0, post.length - 1, in, 0, in.length - 1);
+        return root;
+    }
+
+    public static TreeNode reconstructByPostInHelper(int[] post, int startPost, int endPost, int[] in, int startIn, int endIn){
+        if(startIn > endIn || startPost > endPost) return null;
+        TreeNode root = new TreeNode(post[endPost]);
+        for (int i = startIn; i <= endIn; i++) {
+            if(root.val == in[i]){
+                root.left = reconstructByPostInHelper(post, startPost, i - startIn + startPost - 1, in, startIn, i - 1);
+                root.right = reconstructByPostInHelper(post, i - startIn + startPost, endPost - 1, in, i + 1, endIn );
+                break;
+            }
+        }
+        return root;
+    }
+
+    public static TreeNode reconstructByPreIn_WithoutExtraSpace(int [] pre, int [] in) {
+        if(pre == null || in == null || pre.length == 0 || in.length == 0 || pre.length != in.length) return null;
+        TreeNode root = reconstructByPreInHelper(pre, 0, pre.length - 1, in ,0 , in.length - 1);
+        return root;
+    }
+
+    public static TreeNode reconstructByPreInHelper(int[] pre, int startPre, int endPre, int[] in, int startIn, int endIn){
+        if(startPre > endPre || startIn > endIn) return null;
+        TreeNode root = new TreeNode(pre[startPre]);
+        for (int i = startIn; i <= endIn ; i++) {
+            if (root.val == in[i]){
+                root.left = reconstructByPreInHelper(pre, startPre + 1, i - startIn +startPre, in, startIn, i - 1);
+                root.right = reconstructByPreInHelper(pre, i - startIn + startPre + 1, endPre, in, i + 1, endIn);
+                break;
+            }
+        }
+        return root;
+    }
+
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -16,8 +16,12 @@ public class Test {
     public static void main(String[] args) {
 //        Integer[] a = {1,2,3,4,null,6,7,null,null,null,null,null,null,null,8};
         Integer[] a = {13,65,5,97,25,null,37,22,null,4,28,null,null,32,null};
+        int[] pre =  {13,65,97,22,25,4,28,5,37,32};
+        int[] in =   {22,97,65,4,25,28,13,5,32,37};
+        int[] post = {22,97,4,28,25,65,32,37,5,13};
         BinaryTree bt = new BinaryTree();
         bt.createTree(a);
-        BinaryTree.breadthFirstTraserval(BinaryTree.invertTree(bt.getRoot()));
+        BinaryTree.preoderTraversal(BinaryTree.reconstructByPreIn_WithoutExtraSpace(pre,in));
+
     }
 }
